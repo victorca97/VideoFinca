6208dd18ea30c7bc5cbe95679033d0ef0def9834
JotFe <52864518+JotFe@users.noreply.github.com>
2022-10-20 14:10:16 -0500

    UPDATE

--- main.py
+++ main.py
@@ -9,88 +9,72 @@ app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://localhost/videofincaDB'
 mongo = PyMongo(app)
 
-
 @app.route("/berrios", methods=["GET"])
 def obtener_berrios():
-    propiedades = json_util.dumps(mongo.db.propiedades.find())
-    print(json_util.dumps(propiedades))
-    print(type(json_util.dumps(propiedades)))
+    propiedades = json_util.loads(json_util.dumps(mongo.db.propiedades.find()))[0]
     for propiedad in propiedades:
-                plantilla2=[
-                {
-                "$match": {
-                    "_id": f'{propiedad}'
-                }
-                },  
-                {
-                "$lookup": {
-                    "from": 'plantilla',
-                    "localField": '_id',
-                    "foreignField": 'propiedad',
-                    "as": 'Plantillas'
-                }
-                },  
-                {
-                "$lookup": {
-                    "from": 'propietario',
-                    "localField": '_id',
-                    "foreignField": 'propiedad',
-                    "as": 'Propietarios'
-                }
-                }
-                ]
-                resultados2 =mongo.db.berrios.aggregate(plantilla2)
-    return resultados2
+            plantilla2=[
+            { 
+            "$match": {
+                "_id": 'Finca0001' 
+            }
+            },  
+            {
+            "$lookup": {
+                "from": 'plantilla',
+                "localField": '_id',
+                "foreignField": 'Finca',
+                "as": 'Plantillas'
+            }
+            },  
+            {
+            "$lookup": {
+                "from": 'propietarios',
+                "localField": '_id',
+                "foreignField": 'Finca',
+                "as": 'Propietarios'
+            }
+            }
+            ]
+            resultados2 =mongo.db.propiedades.aggregate(plantilla2)
+            response=json_util.dumps(resultados2)
+    return Response(response,mimetype="application/json"),{"Access-Control-Allow-Origin": "*"}
 
 @app.route("/propiedades", methods=["GET"])
 def obtener_propiedades():
     propiedades = mongo.db.propiedades.find()
     response = json_util.dumps(propiedades)
-    return Response(response, mimetype="application/json")
+    return Response(response, mimetype="application/json"),{"Access-Control-Allow-Origin": "*"}
 
 @app.route("/propiedades", methods=["POST"])
 def crear_propiedad():
-    id_admin = request.json["id_admin"]
-    direccion = request.json["direccion"]
-    nombre = request.json["nombre"]
-    if id_admin or direccion or nombre:
+    _id = request.json["_id"]
+    Admin_Id = request.json["Admin_Id"]
+    Direccion = request.json["Direccion"]
+    Nombre = request.json["Nombre"]
+    if Admin_Id or Direccion or Nombre:
         id = mongo.db.propiedades.insert_one(
-            { "id_admin":id_admin, "direccion":direccion, "nombre":nombre})
+            { "_id":_id,"Admin_Id":Admin_Id, "Direccion":Direccion, "Nombre":Nombre})
         response = {
             "_id":      str(id),
-            "id_admin":id_admin,
-            "direccion":direccion,
-            "nombre":nombre
-        }
-        return response
-    else:
-        return not_found()
-    
-@app.route("/propiedades", methods=["POST"])
-def crear_propiedades():
-    id_propiedad = request.json["id_propiedad"]
-    seccion = request.json["seccion"]
-    if id_propiedad or seccion:
-        id = mongo.db.propiedades.insert_one(
-            { "id_propiedad":id_propiedad, "seccion":seccion})
-        response = {
-            "_id":      str(id),
-            "seccion":  seccion
+            "Admin_Id":Admin_Id,
+            "Direccion":Direccion,
+            "Nombre":Nombre
         }
         return response
     else:
         return not_found()
 
-@app.route("/propiedades", methods=["DELETE"])
-def elimiar_propiedad():
-    mongo.db.propiedades.drop()
-    response = jsonify({'message':' Deleted Successfully'})
+@app.route("/propiedades/<id>", methods=["DELETE"])
+def elimiar_propiedad(id):
+    mongo.db.propiedades.delete_one({'_id': id})
+    response = jsonify({'message': 'User' + id + ' Deleted Successfully'})
     response.status_code = 200
     return response
 
-@app.route("/plantilla", methods=["DELETE"])
-def elimiar_plantilla():
-    mongo.db.plantilla.drop()
+@app.route("/propiedades", methods=["DELETE"])
+def elimiar_propiedades():
+    mongo.db.propiedades.drop()
     response = jsonify({'message':' Deleted Successfully'})
     response.status_code = 200
     return response
@@ -99,41 +83,79 @@ def elimiar_plantilla():
 def obtener_plantilla():
     plantilla = mongo.db.plantilla.find()
     response = json_util.dumps(plantilla)
-    return Response(response, mimetype="application/json")
+    return Response(response, mimetype="application/json"),{"Access-Control-Allow-Origin": "*"}
 
-@app.route("/propietarios", methods=["POST"])
-def crear_porpietarios():
-    id_propiedad = request.json["id_propiedad"]
-    nombres_y_apellidos = request.json["nombres_y_apellidos"]
-    tipo_documento = request.json["tipo_documento"]
-    nro_documento = request.json["nro_documento"]
-    correo_electronico = request.json["correo_electronico"]
-    nro_celular = request.json["nro_celular"]
-    departamento = request.json["departamento"]
-    estacionamiento = request.json["estacionamiento"]
-    if id_propiedad or nombres_y_apellidos or tipo_documento or nro_documento or correo_electronico or nro_celular or departamento or estacionamiento:
-        id = mongo.db.propietarios.insert_one(
-            { "id_propiedad":id_propiedad, "nombres_y_apellidos": nombres_y_apellidos, "tipo_documento": tipo_documento, "nro_documento": nro_documento, "correo_electronico": correo_electronico, "nro_celular": nro_celular, "departamento": departamento, "estacionamiento": estacionamiento})
+@app.route("/plantilla/<id>", methods=["GET"])
+def obtener_plantilla_one(id):
+    plantilla = mongo.db.plantilla.find({'Finca': id, })
+    response = json_util.dumps(plantilla)
+    return Response(response, mimetype="application/json"),{"Access-Control-Allow-Origin": "*"}
+
+@app.route("/plantilla", methods=["POST"])
+def crear_plantilla():
+    Finca = request.json["Finca"]
+    Seccion = request.json["Seccion"]
+    if Finca or Seccion:
+        id = mongo.db.plantilla.insert_one(
+            { "Finca":Finca, "Seccion":Seccion})
         response = {
-            "_id": str(id),
-            "id_propiedad":             id_propiedad,
-            "nombres_y_apellidos":  nombres_y_apellidos,
-            "tipo_documento":       tipo_documento,
-            "nro_documento":        nro_documento,
-            "correo_electronico":   correo_electronico,
-            "nro_celular":          nro_celular,
-            "departamento":         departamento,
-            "estacionamiento":      estacionamiento
+            "_id":      str(id),
+            "Finca":Finca,
+            "Seccion":Seccion,
         }
         return response
     else:
         return not_found()
+
+@app.route("/plantilla", methods=["DELETE"])
+def elimiar_plantilla():
+    mongo.db.plantilla.drop()
+    response = jsonify({'message':' Deleted Successfully'})
+    response.status_code = 200
+    return response
 
 @app.route('/propietarios', methods=['GET'])
 def obtener_propietarios():
     propietarios = mongo.db.propietarios.find()
     response = json_util.dumps(propietarios)
     return Response(response, mimetype="application/json")
+
+@app.route("/propietarios", methods=["POST"])
+def crear_porpietarios():
+    _id = request.json["_id"]
+    Finca = request.json["Finca"]
+    Nombres_y_Apellidos = request.json["Nombres_y_Apellidos"]
+    Tipo_Documento = request.json["Tipo_Documento"]
+    Nro_Documento = request.json["Nro_Documento"]
+    Correo = request.json["Correo"]
+    Telefono = request.json["Telefono"]
+    Departamentos = request.json["Departamentos"]
+    Estacionamientos = request.json["Estacionamientos"]
+    if _id or Finca or Nombres_y_Apellidos or Tipo_Documento or Nro_Documento or Correo or Telefono or Departamentos or Estacionamientos:
+        id = mongo.db.propietarios.insert_one(
+            { "_id":_id,"Finca":Finca, "Nombres_y_Apellidos": Nombres_y_Apellidos, "Tipo_Documento": Tipo_Documento, "Nro_Documento": Nro_Documento, "Correo": Correo, "Telefono": Telefono, "Departamentos": Departamentos, "Estacionamientos": Estacionamientos})
+        response = {
+            "_id": str(id),
+            "_id":                  _id,
+            "Finca":                Finca,
+            "Nombres_y_Apellidos":  Nombres_y_Apellidos,
+            "Tipo_Documento":       Tipo_Documento,
+            "Nro_Documento":        Nro_Documento,
+            "Correo":               Correo,
+            "Telefono":             Telefono,
+            "Departamentos":         Departamentos,
+            "Estacionamientos":      Estacionamientos
+        }
+        return response
+    else:
+        return not_found()
+
+@app.route("/propietarios", methods=["DELETE"])
+def elimiar_propietarios():
+    mongo.db.propietarios.drop()
+    response = jsonify({'message':' Deleted Successfully'})
+    response.status_code = 200
+    return response
 
 @app.route('/propietarios/<id>', methods=['GET'])
 def obtener_propietario(id):
@@ -150,22 +172,26 @@ def eliminar_propietario(id):
 
 @app.route('/propietarios/<_id>', methods=['PUT'])
 def actualizar_porpietario(_id):
-    nombres_y_apellidos = request.json["nombres_y_apellidos"]
-    tipo_documento = request.json["tipo_documento"]
-    nro_documento = request.json["nro_documento"]
-    correo_electronico = request.json["correo_electronico"]
-    nro_celular = request.json["nro_celular"]
-    departamento = request.json["departamento"]
-    estacionamiento = request.json["estacionamiento"]
-    if nombres_y_apellidos or tipo_documento or nro_documento or correo_electronico or nro_celular or departamento or estacionamiento:
+    _id = request.json["_id"]
+    Finca = request.json["Finca"]
+    Nombres_y_Apellidos = request.json["Nombres_y_Apellidos"]
+    Tipo_Documento = request.json["Tipo_Documento"]
+    Nro_Documento = request.json["Nro_Documento"]
+    Correo = request.json["Correo"]
+    Telefono = request.json["Telefono"]
+    Departamentos = request.json["Departamentos"]
+    Estacionamientos = request.json["Estacionamientos"]
+    if _id or Finca or Nombres_y_Apellidos or Tipo_Documento or Nro_Documento or Correo or Telefono or Departamentos or Estacionamientos:
         mongo.db.propietarios.update_one(
-            {'_id': ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)}, {'$set': {"nombres_y_apellidos": nombres_y_apellidos,
-                                                                                            "tipo_documento": tipo_documento,
-                                                                                            "nro_documento": nro_documento,
-                                                                                            "correo_electronico": correo_electronico,
-                                                                                            "nro_celular": nro_celular,
-                                                                                            "departamento": departamento,
-                                                                                            "estacionamiento":estacionamiento}})
+            {'_id': ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)}, {'$set': {  "_id": _id,
+                                                                                            "Finca": Finca,
+                                                                                            "Nombres_y_Apellidos": Nombres_y_Apellidos,
+                                                                                            "Tipo_Documento": Tipo_Documento,
+                                                                                            "Nro_Documento": Nro_Documento,
+                                                                                            "Correo": Correo,
+                                                                                            "Telefono":Telefono,
+                                                                                            "Departamentos":Departamentos,
+                                                                                            "Estacionamientos":Estacionamientos}})
         response = jsonify({'message': 'User' + _id + 'Updated Successfuly'})
         response.status_code = 200
         return response
