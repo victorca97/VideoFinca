278e3fc594cf9796176def7d616e95d51c69c3b9
edube19 <92874291+edube19@users.noreply.github.com>
2022-12-13 11:32:56 -0500

    Add files via upload

--- python/libs/database.py
+++ python/libs/database.py
@@ -6,3 +6,12 @@ def conexion(tabla):
     db = client['videosession']
     colection = db[tabla]
     return colection
+
+"""def conexion(bd,tabla):
+    url_conexion = 'mongodb+srv://Paino:sistemasMONGO@cluster0.awnp8gy.mongodb.net/videosession?retryWrites=true&w=majority'
+    client = MongoClient(url_conexion)
+    db = client[bd]
+    colection = db[tabla]
+    return colection
+
+conexion(admin1,fincas)"""
